3dffef062ba30a4c18321f70d3f29d8c6ea5f4ba
wangbo02 <wangbo02@tudou.com>
2016-11-16 10:03:06 +0800

    add init files

---
+++ js/commons/BottomNav.js
@@ -0,0 +1,26 @@
+'use strict';
+
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
+
+class BottomNav extends Component {
+	render(){
+		return (
+			<View>
+				<Text>首页</Text>
+			</View>
+		);
+	}
+}
+
+class NavItem extends Component {
+	render(){
+		return (
+			<View>
+
+			</View>
+		);
+	}
+}
+
+module.exports = BottomNav;
