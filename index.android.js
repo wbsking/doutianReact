d211fb6233cde1dbd05c89aa1bdb2892d9745551
wangbo02 <wangbo02@tudou.com>
2016-11-17 09:55:22 +0800

    add bottomnav

--- index.android.js
+++ index.android.js
@@ -18,16 +18,6 @@ export default class doutianReact extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          Hello World!
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
         <BottomNav />
       </View>
     );
@@ -37,20 +27,8 @@ export default class doutianReact extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  }
 });
 
 AppRegistry.registerComponent('doutianReact', () => doutianReact);
--- js/commons/BottomNav.js
+++ js/commons/BottomNav.js
@@ -1,13 +1,19 @@
 'use strict';
 
-import React, {Component} from 'react';
-import {Text, View} from 'react-native';
+import React, {Component, Dimensions} from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+
+var width = Dimensions.get('window').width;
 
 class BottomNav extends Component {
 	render(){
 		return (
-			<View>
-				<Text>首页</Text>
+			<View style={styles.bottomNav}>
+				<NavItem text="首页" />
+				<NavItem text="广场" />
+				<NavItem text="发布" />
+				<NavItem text="穿越" />
+				<NavItem text="我的" />
 			</View>
 		);
 	}
@@ -17,10 +23,27 @@ class NavItem extends Component {
 	render(){
 		return (
 			<View>
-
+				<Text style={styles.navItem}>{this.props.text}</Text>
 			</View>
 		);
 	}
 }
+
+const styles = StyleSheet.create({
+	navItem: {
+		color: 'white',
+		fontSize: 14,
+		height: 60,
+		width: width / 5
+	},
+	bottomNav: {
+		width: width,
+		flexDirection:'row',
+		position: "absolute",
+		bottom: 0,
+		height: 60,
+		backgroundColor: "#0ce"
+	}
+});
 
 module.exports = BottomNav;
